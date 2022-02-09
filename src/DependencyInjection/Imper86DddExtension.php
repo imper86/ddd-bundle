fde2da04465b2a56bd4efa7ccef6e6687431f957
imper <me@imper.info>
2022-02-09 15:13:04 +0100

    one another try...

--- src/DependencyInjection/Imper86DddExtension.php
+++ src/DependencyInjection/Imper86DddExtension.php
@@ -7,32 +7,33 @@ namespace Imper86\DddBundle\DependencyInjection;
 use Symfony\Bundle\FrameworkBundle\DependencyInjection\FrameworkExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-final class Imper86DddExtension extends Extension
+final class Imper86DddExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
+    }
 
-        $container->prependExtensionConfig(
-            FrameworkExtension::class,
-            [
-                'framework' => [
-                    'messenger' => [
-                        'default_bus' => 'command.bus',
-                        'buses' => [
-                            'command.bus' => null,
-                            'event.bus' => [
-                                'default_middleware' => 'allow_no_handlers',
-                            ],
-                            'query.bus' => null,
+    public function prepend(ContainerBuilder $container): void
+    {
+        if ($container->hasExtension('framework')) {
+            $container->prependExtensionConfig('framework', [
+                'messenger' => [
+                    'default_bus' => 'command.bus',
+                    'buses' => [
+                        'command.bus' => null,
+                        'event.bus' => [
+                            'default_middleware' => 'allow_no_handlers',
                         ],
+                        'query.bus' => null,
                     ],
                 ],
-            ],
-        );
+            ]);
+        }
     }
 }
