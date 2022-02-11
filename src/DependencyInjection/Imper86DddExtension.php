58f07a0dde30d26fa3146d62940c58c29511466c
imper <me@imper.info>
2022-02-11 17:35:16 +0100

    added autoconfiguration for bus message handlers

--- src/DependencyInjection/Imper86DddExtension.php
+++ src/DependencyInjection/Imper86DddExtension.php
@@ -1,9 +1,12 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Imper86\DddBundle\DependencyInjection;
 
+use Imper86\DDD\Domain\Bus\Command\CommandHandler;
+use Imper86\DDD\Domain\Bus\Event\EventHandler;
+use Imper86\DDD\Domain\Bus\Query\QueryHandler;
 use Symfony\Bundle\FrameworkBundle\DependencyInjection\FrameworkExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -11,29 +14,45 @@ use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-final class Imper86DddExtension extends Extension implements PrependExtensionInterface
+final class Imper86DddExtension extends Extension implements
+    PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yaml');
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . "/../Resources/config")
+        );
+        $loader->load("services.yaml");
     }
 
     public function prepend(ContainerBuilder $container): void
     {
-        if ($container->hasExtension('framework')) {
-            $container->prependExtensionConfig('framework', [
-                'messenger' => [
-                    'default_bus' => 'command.bus',
-                    'buses' => [
-                        'command.bus' => null,
-                        'event.bus' => [
-                            'default_middleware' => 'allow_no_handlers',
+        if ($container->hasExtension("framework")) {
+            $container->prependExtensionConfig("framework", [
+                "messenger" => [
+                    "default_bus" => "command.bus",
+                    "buses" => [
+                        "command.bus" => null,
+                        "event.bus" => [
+                            "default_middleware" => "allow_no_handlers",
                         ],
-                        'query.bus' => null,
+                        "query.bus" => null,
                     ],
                 ],
             ]);
         }
+
+        $container
+            ->registerForAutoconfiguration(CommandHandler::class)
+            ->addTag("messenger.message_handler", ["bus" => "command.bus"]);
+
+        $container
+            ->registerForAutoconfiguration(EventHandler::class)
+            ->addTag("messenger.message_handler", ["bus" => "event.bus"]);
+
+        $container
+            ->registerForAutoconfiguration(QueryHandler::class)
+            ->addTag("messenger.message_handler", ["bus" => "query.bus"]);
     }
 }
