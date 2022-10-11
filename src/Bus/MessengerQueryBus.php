d888821917ff1bae50b8e4b07c690bc9cefceb8b
imper <me@imper.info>
2022-10-11 21:51:40 +0200

    simplify MessengerQueryBus.php according to changes in imper86/ddd

--- src/Bus/MessengerQueryBus.php
+++ src/Bus/MessengerQueryBus.php
@@ -4,11 +4,9 @@ declare(strict_types = 1);
 
 namespace Imper86\DddBundle\Bus;
 
-use Imper86\DDD\Domain\Bus\Exception\InvalidResponseException;
 use Imper86\DDD\Domain\Bus\Exception\NoResponseException;
 use Imper86\DDD\Domain\Bus\Query\Query;
 use Imper86\DDD\Domain\Bus\Query\QueryBus;
-use Imper86\DDD\Domain\Bus\Query\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\HandledStamp;
 
@@ -16,18 +14,14 @@ final class MessengerQueryBus implements QueryBus
 {
     public function __construct(private MessageBusInterface $queryBus) { }
 
-    public function ask(Query $query): ?Response
+    public function ask(Query $query): mixed
     {
         $envelope = $this->queryBus->dispatch($query);
 
         $handledStamp = $envelope->last(HandledStamp::class);
 
         if ($handledStamp instanceof HandledStamp) {
-            $result = $handledStamp->getResult();
-
-            return $result instanceof Response
-                ? $result
-                : throw new InvalidResponseException();
+            return $handledStamp->getResult();
         }
 
         throw new NoResponseException();
